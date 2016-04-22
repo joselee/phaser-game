ffb89910755b2a436a0386168e06dcbc6fd9adde
Jose Lee <jose.lee@basware.com>
2016-04-23 01:54:57 +0300

    create world, add more assets

--- public/scripts/gameModel.ts
+++ public/scripts/gameModel.ts
@@ -1,6 +1,8 @@
 namespace Game {
     export class Game {
+        scale: Phaser.ScaleManager;
         game: Phaser.Game;
+        map: Phaser.Tilemap;
         girl: Phaser.Sprite;
         controls: KeyboardControls;
         lastMove: MovementDirection;
@@ -14,9 +16,31 @@ namespace Game {
         }
 
         preload() {
+            this.game.load.tilemap('world_tilemap', 'assets/world_tilemap.json', null, Phaser.Tilemap.TILED_JSON);
+            this.game.load.image('tileset_nature_1', 'assets/tileset_nature_1.png');
+            this.game.load.image('bush', 'assets/bush.png');
+            this.game.load.image('dead_tree', 'assets/dead_tree.png');
+            this.game.load.image('lilypad_1', 'assets/lilypad_1.png');
+            this.game.load.image('stump', 'assets/stump.png');
+            this.game.load.image('tree', 'assets/tree.png');
+            this.game.load.image('tree_2', 'assets/tree_2.png');
+            this.game.load.image('grass', 'assets/grass.png');
             this.game.load.spritesheet('girl', 'assets/character_girl_5.png', 32, 32);
         }
         create() {
+            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.game.scale.pageAlignHorizontally = true;
+            this.game.scale.pageAlignVertically = true;            
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            
+            this.map = this.game.add.tilemap('world_tilemap');
+            // this.map.addTilesetImage('terrain', 'terrain');
+            // this.map.addTilesetImage('trees', 'trees');
+            // this.map.createLayer('grass_water');
+            // this.map.createLayer('cliffs');
+            // this.map.createLayer('trees_rocks_flowers');
+            
+            
             this.girl = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'girl');
             this.girl.animations.add('walkDown', [0, 1, 2]);
             this.girl.animations.add('walkLeft', [3, 4, 5]);
--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/angular-ui-bootstrap/index.d.ts" />
-/// <reference path="main/ambient/angular/index.d.ts" />
-/// <reference path="main/ambient/bootstrap/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
-/// <reference path="main/ambient/lodash/index.d.ts" />
