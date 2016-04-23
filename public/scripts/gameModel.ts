705521c8f2b0c9adfd5aed4e14808c6d2fdd37ef
Jose Lee <jose.lee@basware.com>
2016-04-23 13:43:32 +0300

    Load world and layers, modify controls for diagonal move, and wsasd

--- public/scripts/gameModel.ts
+++ public/scripts/gameModel.ts
@@ -3,9 +3,12 @@ namespace Game {
         scale: Phaser.ScaleManager;
         game: Phaser.Game;
         map: Phaser.Tilemap;
+        bgLayer: Phaser.TilemapLayer;
+        rocksLayer: Phaser.TilemapLayer;
+        plantsLayer: Phaser.TilemapLayer;
+        blockedLayer: Phaser.TilemapLayer;
         girl: Phaser.Sprite;
         controls: KeyboardControls;
-        lastMove: MovementDirection;
 
         constructor() {
             this.game = new Phaser.Game(600, 400, Phaser.AUTO, 'game', {
@@ -18,30 +21,25 @@ namespace Game {
         preload() {
             this.game.load.tilemap('world_tilemap', 'assets/world_tilemap.json', null, Phaser.Tilemap.TILED_JSON);
             this.game.load.image('tileset_nature_1', 'assets/tileset_nature_1.png');
-            this.game.load.image('bush', 'assets/bush.png');
-            this.game.load.image('dead_tree', 'assets/dead_tree.png');
-            this.game.load.image('lilypad_1', 'assets/lilypad_1.png');
-            this.game.load.image('stump', 'assets/stump.png');
-            this.game.load.image('tree', 'assets/tree.png');
-            this.game.load.image('tree_2', 'assets/tree_2.png');
-            this.game.load.image('grass', 'assets/grass.png');
             this.game.load.spritesheet('girl', 'assets/character_girl_5.png', 32, 32);
         }
         create() {
-            this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-            this.game.scale.pageAlignHorizontally = true;
-            this.game.scale.pageAlignVertically = true;            
-            this.game.physics.startSystem(Phaser.Physics.ARCADE);
-            
+            // Load the tilemap world_tilemap.json, and the image asset(s) it needs 
             this.map = this.game.add.tilemap('world_tilemap');
-            // this.map.addTilesetImage('terrain', 'terrain');
-            // this.map.addTilesetImage('trees', 'trees');
-            // this.map.createLayer('grass_water');
-            // this.map.createLayer('cliffs');
-            // this.map.createLayer('trees_rocks_flowers');
-            
-            
-            this.girl = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'girl');
+            this.map.addTilesetImage('tileset_nature_1', 'tileset_nature_1');
+
+            // Render the layers- IDs are defined in the tilemap
+            this.bgLayer = this.map.createLayer('bgLayer');
+            this.rocksLayer = this.map.createLayer('rocksLayer');
+            this.plantsLayer = this.map.createLayer('plantsLayer');
+            this.blockedLayer = this.map.createLayer('blockedLayer');
+
+            // set blockedLayer as invisible collision layer
+            this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
+            this.blockedLayer.alpha = 0;
+            this.bgLayer.resizeWorld(); // set world boundaries to map size
+
+            this.girl = this.game.add.sprite(180, 300, 'girl');
             this.girl.animations.add('walkDown', [0, 1, 2]);
             this.girl.animations.add('walkLeft', [3, 4, 5]);
             this.girl.animations.add('walkRight', [6, 7, 8]);
@@ -51,33 +49,56 @@ namespace Game {
                 up: this.game.input.keyboard.addKey(Phaser.Keyboard.UP),
                 down: this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
                 left: this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
-                right: this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
+                right: this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+                w: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
+                s: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
+                a: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
+                d: this.game.input.keyboard.addKey(Phaser.Keyboard.D)
             };
+
+            //the camera will follow the player in the world
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            this.game.physics.arcade.enable(this.girl);
+            this.game.camera.follow(this.girl);
         }
         update() {
-            if (this.controls.up.isDown) {
-                this.girl.animations.play('walkUp', 10, true);
-                this.girl.y--;
-                this.lastMove = MovementDirection.up;
+            let up = this.controls.up.isDown       || this.controls.w.isDown;
+            let down = this.controls.down.isDown   || this.controls.s.isDown;
+            let left = this.controls.left.isDown   || this.controls.a.isDown;
+            let right = this.controls.right.isDown || this.controls.d.isDown;
+
+            this.girl.body.velocity.y = 0;
+            this.girl.body.velocity.x = 0;
+            
+            if (up) {
+                if(!left && !right){
+                    this.girl.animations.play('walkUp', 10, true);
+                }
+                this.girl.body.velocity.y = -150;
             }
-            else if (this.controls.down.isDown) {
-                this.girl.y++;
-                this.girl.animations.play('walkDown', 10, true);
-                this.lastMove = MovementDirection.down;
+            else if (down) {
+                if(!left && !right){
+                    this.girl.animations.play('walkDown', 10, true);
+                }
+                this.girl.body.velocity.y = 150;
             }
-            else if (this.controls.left.isDown) {
-                this.girl.x--;
+
+            if (left) {
                 this.girl.animations.play('walkLeft', 10, true);
-                this.lastMove = MovementDirection.left;
+                this.girl.body.velocity.x = -150;
             }
-            else if (this.controls.right.isDown) {
-                this.girl.x++;
+            else if (right) {
                 this.girl.animations.play('walkRight', 10, true);
-                this.lastMove = MovementDirection.right;
+                this.girl.body.velocity.x = 150;
             }
-            else {
+
+            if (!up && !down && !left && !right) {
                 this.girl.animations.stop();
             }
+
+            this.game.physics.arcade.collide(this.girl, this.blockedLayer);
+            // this.game.physics.arcade.overlap(this.girl, this.items, this.collect, null, this);
+            // this.game.physics.arcade.overlap(this.girl, this.doors, this.enterDoor, null, this);
         }
     }
 
@@ -86,7 +107,9 @@ namespace Game {
         down: Phaser.Key;
         left: Phaser.Key;
         right: Phaser.Key;
+        w: Phaser.Key;
+        s: Phaser.Key;
+        a: Phaser.Key;
+        d: Phaser.Key;
     }
-
-    enum MovementDirection { up, down, left, right }
 }
