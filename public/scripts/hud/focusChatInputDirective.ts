6d073b15dae0217e191502dd1279e0465ef38dc5
Jose Lee <jose.lee@basware.com>
2016-04-24 03:03:20 +0300

    Fix bug where game consumes input if the chat input is put to focus manually by click

--- public/scripts/hud/focusChatInputDirective.ts
+++ public/scripts/hud/focusChatInputDirective.ts
@@ -2,26 +2,25 @@ namespace PhaserGame {
     function FocusChatInputDirective ($rootScope, $timeout) {
         return {
             restrict: 'A',
-            link: function($scope, $element, $attr) {
+            link: ($scope, $element, $attr) => {
                 let chatFocus = false;
                 angular.element(document).on('keypress', (e)=> {
                     // Enter key toggles game key capture and chat HUD
                     if(e.keyCode === 13) {
-                        if(!chatFocus){
-                            chatFocus = true;
-                            $timeout(() => { $element.focus(); });
-                        }
-                        else {
-                            chatFocus = false;
-                            $timeout(() => { $element.blur(); });
-                        }
+                        $timeout(() => {
+                            if (chatFocus)
+                                $element.blur();
+                            else
+                                $element.focus();
+                        });
                     }
                 });
 
-                // Gets triggered when user clicks on/off input, or presses enter
-                $element.on('focus blur', () => {
+                // Gets triggered when user clicks on/off input, or presses enter (above bindings)
+                $element.on('focus blur', (e) => {
+                    chatFocus = (e.type === 'focus');
                     $rootScope.$emit('setFocusToChat', chatFocus);
-                });
+                }));
 
                 // Disable tab key, so we dont lose focus while typing
                 $element.on('keydown', (e) => {
