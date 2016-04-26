140ce170375eb3d309ade73399a69974183e5250
Jose Lee <jose.lee@basware.com>
2016-04-26 13:23:23 +0300

    raw canvas realtime with sio

---
+++ server.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+
+app.use('/', express.static(__dirname + '/public'));
+server.listen(8080);
+console.log("Server started on port 8080");
+
+var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function (id) {
+    var self = {
+        x: 250,
+        y: 250,
+        id: id,
+        number: "" + Math.floor(10 * Math.random()),
+        pressingRight: false,
+        pressingLeft: false,
+        pressingUp: false,
+        pressingDown: false,
+        maxSpd: 5,
+    }
+    self.updatePosition = function () {
+        if (self.pressingRight)
+            self.x += self.maxSpd;
+        if (self.pressingLeft)
+            self.x -= self.maxSpd;
+        if (self.pressingUp)
+            self.y -= self.maxSpd;
+        if (self.pressingDown)
+            self.y += self.maxSpd;
+    }
+    return self;
+}
+
+var io = require('socket.io')(server, {});
+io.sockets.on('connection', function (socket) {
+    socket.id = Math.random();
+    SOCKET_LIST[socket.id] = socket;
+
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
+
+    socket.on('disconnect', function () {
+        delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
+    });
+
+    socket.on('keyPress', function (data) {
+        if (data.inputId === 'left')
+            player.pressingLeft = data.state;
+        else if (data.inputId === 'right')
+            player.pressingRight = data.state;
+        else if (data.inputId === 'up')
+            player.pressingUp = data.state;
+        else if (data.inputId === 'down')
+            player.pressingDown = data.state;
+    });
+});
+
+setInterval(function () {
+    var pack = [];
+    for (var i in PLAYER_LIST) {
+        var player = PLAYER_LIST[i];
+        player.updatePosition();
+        pack.push({
+            x: player.x,
+            y: player.y,
+            number: player.number
+        });
+    }
+    for (var i in SOCKET_LIST) {
+        var socket = SOCKET_LIST[i];
+        socket.emit('newPositions', pack);
+    }
+}, 1000/60);
