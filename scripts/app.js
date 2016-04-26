c91ad85b3325bea16722b089ac9eb3bb752e3313
Jose Lee <jose.lee@basware.com>
2016-04-26 19:41:55 +0300

    Separate code in Create() function to logical blocks

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
-var player;
 var map;
-var bgLayer;
+var player;
 var controls;
-
 var game = new Phaser.Game(480, 320, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
@@ -11,10 +9,42 @@ function preload() {
     game.load.spritesheet('player', 'assets/player.png', 32, 32);
 }
 function create() {
+    setupMap();
+    setupPlayer();
+    setupControls();
+}
+function update() {
+    var x = player.body.velocity.x;
+    var y = player.body.velocity.y;
+
+    // Vertical movement
+    if (y === controls.up.velocity) {
+        player.animations.play(controls.up.animation, 10, true);
+    }
+    else if (y === controls.down.velocity) {
+        player.animations.play(controls.down.animation, 10, true);
+    }
+
+    // Horizontal movement
+    if (x === controls.left.velocity && y === 0) {
+        player.animations.play(controls.left.animation, 10, true);
+    }
+    else if (x === controls.right.velocity && y === 0) {
+        player.animations.play(controls.right.animation, 10, true);
+    }
+
+    // No movement
+    if (x === 0 && y === 0) {
+        player.animations.stop();
+    }
+}
+
+function setupMap() {
     map = game.add.tilemap('world');
     map.addTilesetImage('worldpng', 'worldpng');
-    bgLayer = map.createLayer('bg');
-
+    map.createLayer('bg');
+}
+function setupPlayer() {
     player = game.add.sprite(240, 160, 'player');
     player.animations.add('walkDown', [0, 1, 2]);
     player.animations.add('walkLeft', [3, 4, 5]);
@@ -23,7 +53,8 @@ function create() {
     player.anchor.x = 0.5;
     player.anchor.y = 0.5;
     game.physics.arcade.enable(player);
-
+}
+function setupControls() {
     controls = {
         up: {
             key: game.input.keyboard.addKey(Phaser.Keyboard.UP),
@@ -50,7 +81,6 @@ function create() {
             animation: 'walkRight'
         }
     }
-
     _.each(controls, function (control) {
         control.key.onDown.add(function () {
             player.body.velocity[control.direction] += control.velocity;
@@ -59,29 +89,4 @@ function create() {
             player.body.velocity[control.direction] -= control.velocity;
         });
     });
-}
-function update() {
-    var x = player.body.velocity.x;
-    var y = player.body.velocity.y;
-
-    // Vertical movement
-    if (y === controls.up.velocity) {
-        player.animations.play(controls.up.animation, 10, true);
-    }
-    else if (y === controls.down.velocity) {
-        player.animations.play(controls.down.animation, 10, true);
-    }
-
-    // Horizontal movement
-    if (x === controls.left.velocity && y === 0) {
-        player.animations.play(controls.left.animation, 10, true);
-    }
-    else if (x === controls.right.velocity && y === 0) {
-        player.animations.play(controls.right.animation, 10, true);
-    }
-
-    // No movement
-    if (x === 0 && y === 0) {
-        player.animations.stop();
-    }
 }
