d08f6bba872a81c590176294f17129ac402f6266
Jose Lee <jose.lee@basware.com>
2016-04-27 09:11:37 +0300

    nevermind

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').Server(app);
 
 app.use('/', express.static(__dirname + '/public'));
-server.listen(80);
+server.listen(8080);
 console.log("Server started on port 8080");
 
 var SOCKET_LIST = {};
