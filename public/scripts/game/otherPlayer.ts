632ea4c0cc005818037e6630ce1cb6cfe6ad91f1
Jose Lee <jose.lee@basware.com>
2016-04-29 17:00:43 +0300

    Other player movement animatinos added

--- public/scripts/game/otherPlayer.ts
+++ public/scripts/game/otherPlayer.ts
@@ -2,13 +2,12 @@ namespace PhaserGame {
     export class OtherPlayer extends Phaser.Sprite {
         game: IPhaserAngularGame;
         animationSpeed: number = 10;
-        movementSpeed: number = 100;
-        lastPlayerData: IPlayerData;
         playerId: string;
 
         constructor(game: IPhaserAngularGame, playerData:IPlayerData, spriteSheetId: string, private mapLayers: IMapLayers) {
             super(game, playerData.posX, playerData.posY, spriteSheetId);
             this.game = game;
+            this.game.physics.arcade.enable(this);
             this.playerId = playerData.id;
 
             this.animations.add('walkDown', [0, 1, 2]);
@@ -16,27 +15,14 @@ namespace PhaserGame {
             this.animations.add('walkRight', [6, 7, 8]);
             this.animations.add('walkUp', [9, 10, 11]);
         }
-
+        
         movePlayer(playerData: IPlayerData) {
             this.position.x = playerData.posX;
             this.position.y = playerData.posY;
-        }
-        animateMovement(velocity) {
-            // Vertical movement
-            if (velocity.y < 0)
-                this.animations.play('walkUp', this.animationSpeed, true);
-            else if (velocity.y > 0)
-                this.animations.play('walkDown', this.animationSpeed, true);
-
-            // Horizontal movement
-            if (velocity.x < 0 && velocity.y === 0)
-                this.animations.play('walkLeft', this.animationSpeed, true);
-            else if (velocity.x > 0 && velocity.y === 0)
-                this.animations.play('walkRight', this.animationSpeed, true);
-
-            // No movement
-            if (velocity.x === 0 && velocity.y === 0)
+            this.animations.play(playerData.animation, this.animationSpeed, true);
+            if (!playerData.animationPlaying) {
                 this.animations.stop();
+            }
         }
     }
 }
