2a61254978a4ea3d8928e9b072426121fd0d7ed8
Jose Lee <jose.lee@basware.com>
2016-05-01 01:43:05 +0300

    Tree with multi layers. Treetop rendered above player sprite. fine collision

--- scripts/app.js
+++ scripts/app.js
@@ -1,92 +1,85 @@
 var map;
 var player;
 var controls;
+var layers = {};
 var game = new Phaser.Game(480, 320, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
 
 function preload() {
-    game.load.tilemap('world', 'assets/world.json', null, Phaser.Tilemap.TILED_JSON);
+    // Load map json, and all pngs used by map
+    game.load.tilemap('world', 'assets/world2.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('worldpng', 'assets/worldpng.png');
+    game.load.image('treetrunk', 'assets/treetrunk.png');
+    game.load.image('treetop', 'assets/treetop.png');
+    game.load.image('collision', 'assets/collision.png');
     game.load.spritesheet('player', 'assets/player.png', 32, 32);
 }
 function create() {
-    setupMap();
-    setupPlayer();
-    setupControls();
-}
-function update() {
-    var x = player.body.velocity.x;
-    var y = player.body.velocity.y;
-
-    // Vertical movement
-    if (y === controls.up.velocity) {
-        player.animations.play(controls.up.animation, 10, true);
-    }
-    else if (y === controls.down.velocity) {
-        player.animations.play(controls.down.animation, 10, true);
-    }
-
-    // Horizontal movement
-    if (x === controls.left.velocity && y === 0) {
-        player.animations.play(controls.left.animation, 10, true);
-    }
-    else if (x === controls.right.velocity && y === 0) {
-        player.animations.play(controls.right.animation, 10, true);
-    }
-
-    // No movement
-    if (x === 0 && y === 0) {
-        player.animations.stop();
-    }
-}
-
-function setupMap() {
     map = game.add.tilemap('world');
     map.addTilesetImage('worldpng', 'worldpng');
-    map.createLayer('bg');
-}
-function setupPlayer() {
-    player = game.add.sprite(240, 160, 'player');
+    map.addTilesetImage('treetrunk', 'treetrunk');
+    map.addTilesetImage('treetop', 'treetop');
+    map.addTilesetImage('collision', 'collision');
+
+    // Order of creating layers and adding sprites matter for z-index
+    layers.bg = map.createLayer('bg');
+    layers.treeTrunk = map.createLayer('treeTrunk');
+    player = game.add.sprite(320, 160, 'player');
+    layers.treeTop = map.createLayer('treeTop');
+    layers.collision = map.createLayer('collision');
+    layers.collision.alpha = 0;
+    map.setCollisionBetween(1, 2000, true, 'collision');
+    game.physics.arcade.enable(player);
+
     player.animations.add('walkDown', [0, 1, 2]);
     player.animations.add('walkLeft', [3, 4, 5]);
     player.animations.add('walkRight', [6, 7, 8]);
     player.animations.add('walkUp', [9, 10, 11]);
     player.anchor.x = 0.5;
     player.anchor.y = 0.5;
-    game.physics.arcade.enable(player);
-}
-function setupControls() {
+
     controls = {
-        up: {
-            key: game.input.keyboard.addKey(Phaser.Keyboard.UP),
-            direction: 'y',
-            velocity: -100,
-            animation: 'walkUp'
-        },
-        down: {
-            key: game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
-            direction: 'y',
-            velocity: 100,
-            animation: 'walkDown'
-        },
-        left: {
-            key: game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
-            direction: 'x',
-            velocity: -100,
-            animation: 'walkLeft'
-        },
-        right: {
-            key: game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
-            direction: 'x',
-            velocity: 100,
-            animation: 'walkRight'
-        }
+        up: game.input.keyboard.addKey(Phaser.Keyboard.UP),
+        down: game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
+        left: game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
+        right: game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+        w: game.input.keyboard.addKey(Phaser.Keyboard.W),
+        s: game.input.keyboard.addKey(Phaser.Keyboard.S),
+        a: game.input.keyboard.addKey(Phaser.Keyboard.A),
+        d: game.input.keyboard.addKey(Phaser.Keyboard.D)
+    };
+}
+function update() {
+    game.physics.arcade.collide(player, layers.collision);
+
+    player.body.velocity.y = 0;
+    player.body.velocity.x = 0;
+
+    var up = controls.up.isDown || controls.w.isDown;
+    var down = controls.down.isDown || controls.s.isDown;
+    var left = controls.left.isDown || controls.a.isDown;
+    var right = controls.right.isDown || controls.d.isDown;
+
+    if (up) {
+        player.animations.play('walkUp', 10, true);
+        player.body.velocity.y = -100;
+    } else if (down) {
+        player.animations.play('walkDown', 10, true);
+        player.body.velocity.y = 100;
     }
-    _.each(controls, function (control) {
-        control.key.onDown.add(function () {
-            player.body.velocity[control.direction] += control.velocity;
-        });
-        control.key.onUp.add(function () {
-            player.body.velocity[control.direction] -= control.velocity;
-        });
-    });
+
+    if (left) {
+        if (!up && !down) {
+            player.animations.play('walkLeft', 10, true);
+        }
+        player.body.velocity.x = -100;
+    } else if (right) {
+        if (!up && !down) {
+            player.animations.play('walkRight', 10, true);
+        }
+        player.body.velocity.x = 100;
+    }
+
+    if (!up && !down && !left && !right) {
+        player.animations.stop();
+    }
 }
